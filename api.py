2e37c3f01c024a5deae8780299a0a0bb3912901b
liaoshaolim <366688603@qq.com>
2020-12-15 11:07:09 +0800

    feat：暂注释掉用不到的代码

--- api.py
+++ api.py
@@ -1,20 +1,20 @@
 import flask
 from spider_price import format_response_data
-import ast
-from flask_cors import CORS
+# import ast
+# from flask_cors import CORS
 
 server = flask.Flask(__name__)
-CORS(server, resources=r'*')
+# CORS(server, resources=r'*')
 
 
 @server.route('/get_data', methods=['get'])
 def api_data():
-    data_bytes = flask.request.get_data()
-    data_str = str(data_bytes, encoding='utf-8')
-    data_dic = None
-    if len(data_str) > 0:
-        data_dic = ast.literal_eval(data_str)
-    data = format_response_data(req=data_dic)
+    # data_bytes = flask.request.get_data()
+    # data_str = str(data_bytes, encoding='utf-8')
+    # data_dic = None
+    # if len(data_str) > 0:
+    #     data_dic = ast.literal_eval(data_str)
+    data = format_response_data()
     return data
 
 
